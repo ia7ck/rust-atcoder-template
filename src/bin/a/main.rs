b5fa4bb550a65ea66ef4e03893f0f64bf69e9a5a
aikd <23146842+ia7ck@users.noreply.github.com>
2021-07-04 20:04:30 +0900

    Create main.rs

---
+++ src/bin/a/main.rs
@@ -0,0 +1,10 @@
+use procon_reader::ProconReader;
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut rd = ProconReader::new(stdin.lock());
+
+    let n: usize = rd.get();
+
+    println!("n = {}", n);
+}
