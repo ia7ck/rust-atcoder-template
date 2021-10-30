9a40f87e72a84da6ade3002dea0f71403d08fb26
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-10-31 02:54:15 +0900

    update macro

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use input_i_scanner::{InputIScanner, scan_with};
+use input_i_scanner::InputIScanner;
 
 fn main() {
     let stdin = std::io::stdin();
     let mut _i_i = InputIScanner::from(stdin.lock());
 
-    let n = scan_with!(_i_i, usize);
+    macro_rules! scan {
+        (($($t: ty),+)) => {
+            ($(scan!($t)),+)
+        };
+        ($t: ty) => {
+            _i_i.scan::<$t>() as $t
+        };
+        (($($t: ty),+); $n: expr) => {
+            std::iter::repeat_with(|| scan!(($($t),+))).take($n).collect::<Vec<_>>()
+        };
+        ($t: ty; $n: expr) => {
+            std::iter::repeat_with(|| scan!($t)).take($n).collect::<Vec<_>>()
+        };
+    }
+
+    let n = scan!(usize);
     todo!();
 }
