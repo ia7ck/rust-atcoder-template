3d61f885ea559d1fa8c0a09b3a9797af67b78b82
ia7ck <23146842+ia7ck@users.noreply.github.com>
2022-02-11 16:47:24 +0900

    update scanner

--- src/bin/a/main.rs
+++ src/bin/a/main.rs
@@ -1,24 +1,7 @@
-use input_i_scanner::InputIScanner;
+use scanner_proc_macro::insert_scanner;
 
+#[insert_scanner]
 fn main() {
-    let stdin = std::io::stdin();
-    let mut _i_i = InputIScanner::from(stdin.lock());
-
-    macro_rules! scan {
-        (($($t: ty),+)) => {
-            ($(scan!($t)),+)
-        };
-        ($t: ty) => {
-            _i_i.scan::<$t>() as $t
-        };
-        (($($t: ty),+); $n: expr) => {
-            std::iter::repeat_with(|| scan!(($($t),+))).take($n).collect::<Vec<_>>()
-        };
-        ($t: ty; $n: expr) => {
-            std::iter::repeat_with(|| scan!($t)).take($n).collect::<Vec<_>>()
-        };
-    }
-
     let n = scan!(usize);
     todo!();
 }
